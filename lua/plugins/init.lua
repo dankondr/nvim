496100eb699bbe1bf3f3ea6ffdbbfa3920550ac5
dankondr <dankondr@icloud.com>
2025-06-19 14:08:44 +0300

    Updates

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -36,39 +36,8 @@ return {
       })
     end,
     event = {"CmdlineEnter"},
-    ft = {"go", 'gomod'},
+    ft = {"go", "gomod"},
     build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-  },
-
-  {
-    "tris203/precognition.nvim",
-    --event = "VeryLazy",
-    opts = {
-      startVisible = true,
-      showBlankVirtLine = true,
-      highlightColor = { link = "Comment" },
-      hints = {
-           Caret = { text = "^", prio = 2 },
-           Dollar = { text = "$", prio = 1 },
-           MatchingPair = { text = "%", prio = 5 },
-           Zero = { text = "0", prio = 1 },
-           w = { text = "w", prio = 10 },
-           b = { text = "b", prio = 9 },
-           e = { text = "e", prio = 8 },
-           W = { text = "W", prio = 7 },
-           B = { text = "B", prio = 6 },
-           E = { text = "E", prio = 5 },
-      },
-      gutterHints = {
-          G = { text = "G", prio = 10 },
-          gg = { text = "gg", prio = 9 },
-          PrevParagraph = { text = "{", prio = 8 },
-          NextParagraph = { text = "}", prio = 8 },
-      },
-      disabled_fts = {
-          "startify",
-      },
-    },
   },
 
   {
@@ -109,7 +78,7 @@ return {
        "html", "css", "go", "rust",
        "typescript", "kotlin", "javascript",
        "yaml", "json", "cpp", "c", "cmake",
-       "python"
+       "python", "markdown", "dockerfile",
   		},
   	},
   },
