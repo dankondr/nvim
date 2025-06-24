649a7a159200d0200cf010c3124aee58a7da207c
dankondr <dankondr@icloud.com>
2025-06-24 18:36:51 +0300

    Better

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,11 +3,26 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local autocmd = vim.api.nvim_create_autocmd
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 map("n", "grr", require("telescope.builtin").lsp_references)
 map("n", "gi", require("telescope.builtin").lsp_implementations)
+map("n", "gd", require("telescope.builtin").lsp_definitions)
+map("n", "gt", require("telescope.builtin").lsp_type_definitions)
+map("n", "<leader>fs", require("telescope.builtin").lsp_document_symbols)
+
+autocmd({'CursorHold', 'CursorHoldI'}, {
+  callback = function (_)
+    vim.lsp.buf.document_highlight()
+  end
+})
+autocmd({'CursorMoved'}, {
+  callback = function (_)
+    vim.lsp.buf.clear_references()
+  end
+})
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -105,4 +105,22 @@ return {
     },
   },
 
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>tt",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>tT",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+    },
+  },
+
 }
